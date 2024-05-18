a179681dd77de023279af58d1aafb0df47634b80
FantasWolf <jaganhari2002@gmail.com>
2024-05-18 18:51:39 +0530

    Stopped web3.0

--- Web3.0/SmartContract/hardhat.config.js
+++ Web3.0/SmartContract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-sepolia.g.alchemy.com/v2/5CqYx1hIX6Z4FlKr0QguyNOCCTFaQfTK
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/5CqYx1hIX6Z4FlKr0QguyNOCCTFaQfTK',
+      accounts: ['853843005407114687233c29448018b8f2d870a05fe3f6bf0f4229fced3ce710'],
+    }
+  }
 };
